8aac28a15b40cc15b3e632cf538ec1e3c1a03149
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-08-21 09:11:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
-let usuario = []
+let usuarios = []
 
-function adicionarUsuario(nome, idade, email){
-    nome = "João"
-    idade = 17
-    email = "joaosla@gmail.com"
+function adicionarUsuario(nome, idade, email) {
+    const usuario = {
+        nome,
+        idade,
+        email
+    }
 
-    if (idade > 0 && nome && email){
-        usuario.push(nome)
-    }else{
-        console.log("Não foi possivel fazer o seu cadastro!")
+    if(idade > 0){
+        console.log("Sua idade é valida")
+    } else{
+        console.log("Sua idade é inválida. Deve ser um número maior que zero!")
+        return
+    }
+
+    if(nome === undefined && email === undefined && email === undefined) {
+        console.log("Todos os dados estão faltando.")
+        return
+    } else if(nome === undefined || email === undefined || email === undefined){
+        console.log("Há dados faltando.")
+        return
+    } else{
+        usuarios.push(usuario)
+        console.log(`Usuário ${nome} adicionado com sucesso!`)
+    }
+    
+    
+    listarUsuarios(usuarios);
+}
+
+function listarUsuarios(usuarios) {
+    if (usuarios.length === 0) {
+        console.log("Nenhum usuário cadastrado.")
+        return
+    } else {
+        console.log(`Usuários cadastrados: ${usuarios.length}`)
+        usuarios.forEach((usuario, index) => {
+            console.log(`Nome: ${usuario.nome}, Idade: ${usuario.idade}, Email: ${usuario.email}`)
+        })
     }
 }
 
-function listarusuarios(){
-    usuario.forEach(function(usuarios){
-        console.log("Esses são os usuarios cadastrados no nosso site:\n" + usuarios)
-    })
-}
-
-adicionarUsuario()
-listarusuarios()
+adicionarUsuario('João', 17 , 'joaosla@gmail.com')
